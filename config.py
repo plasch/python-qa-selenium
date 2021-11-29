2b537038bce2a8506fd7b75f5293eebbdae8fb2c
plasch <>
2021-11-29 23:33:41 +0300

    add web elements

--- config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-DRIVERS_FOLDER = os.path.expanduser("~/Dev/drivers")
-CHROMEDRIVER = f"{DRIVERS_FOLDER}/chromedriver"
-GECKODRIVER = f"{DRIVERS_FOLDER}/geckodriver"
-OPERADRIVER = f"{DRIVERS_FOLDER}/operadriver"
